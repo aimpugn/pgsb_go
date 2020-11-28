a74d93e5de7dda72332f462c731574791d380e64
aimpugn <aimpugn@gmail.com>
2020-11-28 17:47:49 +0900

    간단한 http 서버

--- microservice.go
+++ microservice.go
@@ -9,6 +9,7 @@ import (
 // If can not access this go server via browser, before move forward, check firewalld rather than iptables
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/api/echo", echo)
 	err := http.ListenAndServe(port(), nil)
 	fmt.Println(err)
 }
@@ -26,4 +27,12 @@ func port() string {
 func index(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Hello Cloud Native Go.")
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	fmt.Println("query: ", query)
+	message := query["message"][0]
+	w.Header().Add("Content-Type", "text/plain")
+	fmt.Fprintf(w, message)
 }
