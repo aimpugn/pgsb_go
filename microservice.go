fb4af0d3c417a37b80a944e680a3d5bdb865854a
aimpugn <aimpugn@gmail.com>
2020-11-28 17:36:14 +0900

    간단한 http 서버

--- microservice.go
+++ microservice.go
@@ -3,13 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 // If can not access this go server via browser, before move forward, check firewalld rather than iptables
 func main() {
 	http.HandleFunc("/", index)
-	err := http.ListenAndServe(":8089", nil)
+	err := http.ListenAndServe(port(), nil)
 	fmt.Println(err)
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	fmt.Println(port)
+	if len(port) == 0 {
+		port = "8089"
+	}
+
+	return ":" + port
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
